d3f4acd30a2e5029a39f6907a7c60b2685c36956
Noah Katz <noahkatz@chicagobooth.edu>
2023-04-20 15:59:05 +0000

    eldest!

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,4 +3,8 @@ class DirectorsController < ApplicationController
     @list_of_directors = Director.all
     render({:template => "directors_template/index.html.erb"})
   end
+  def wisest
+    @oldest = Director.where.not({ :dob => nil}).order({:dob => :asc}).at(0)
+    render({:template => "directors_template/eldest.html.erb"})
+  end
 end

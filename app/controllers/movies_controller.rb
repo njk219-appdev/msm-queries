b8c5ccb4df563b4180767b5c5f21e7339e94555a
Noah Katz <noahkatz@chicagobooth.edu>
2023-04-26 01:01:48 +0000

    Movies and Directors complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,6 @@
 class MoviesController < ApplicationController
   def movieindex
     @list_of_movies = Movie.all
-    #the_id = params.fetch("an_id")
-    #@the_director = Director.where({:id => the_id}).at(0)
     render({:template => "movie_template/movieindex.html.erb"})
   end
   def movie_details
@@ -10,7 +8,6 @@ class MoviesController < ApplicationController
 
     the_id = params.fetch("an_id")
     @the_movie = Movie.where({:id => the_id}).at(0)
-    #@filmography = Movie.where({:movie_id => the_id})
     render({:template => "movie_template/details.html.erb"})
   end
 end
